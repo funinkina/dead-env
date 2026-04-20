0040e05f1594a75e90c3024b267c8d722de71e07
abhiruchipatilbhagat <abhipatilngp@gmail.com>
2026-04-20 10:42:18 +0530

    modified:   internal/profile/profile.go

--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -1,1 +1,11 @@
 package profile
+
+import (
+	"funinkina/deadenv/internal/history"
+	"funinkina/deadenv/internal/keychain"
+)
+
+type ProfileService struct {
+	store    keychain.Store
+	recorder history.Recorder
+}
