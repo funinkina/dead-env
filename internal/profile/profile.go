9f51b3b2f1254a9fb98e84a75725c2746bfdef5b
abhiruchipatilbhagat <abhipatilngp@gmail.com>
2026-04-20 10:58:23 +0530

    Co-authored-by: Aryan Kushwaha <hello@funinkina.co.in>

--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -9,3 +9,12 @@ type ProfileService struct {
 	store    keychain.Store
 	recorder history.Recorder
 }
+
+func NewProfileService(store keychain.Store, recorder history.Recorder) *ProfileService {
+	
+}
+
+// helper function
+func getServiceName(profile string) string {
+	return "deadenv/" + profile
+}
