b02018e0f4d046890b508ae6b4471fcdf6a447fc
abhiruchipatilbhagat <abhipatilngp@gmail.com>
2026-04-20 12:12:42 +0530

    modified:   internal/profile/profile.go

--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -11,10 +11,22 @@ type ProfileService struct {
 }
 
 func NewProfileService(store keychain.Store, recorder history.Recorder) *ProfileService {
-	
+	return &ProfileService{store, recorder}
 }
 
 // helper function
 func getServiceName(profile string) string {
 	return "deadenv/" + profile
+}
+
+func SetKey(profile, key, value string) error {
+	service := getServiceName(profile)
+	store.Write(service, key, value)
+}
+
+func hashValue(value string) string {
+	for idx, char := range value {
+		char = char + rune(idx)
+	}
+	return string(value)
 }
